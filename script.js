049ba787202243a521a57fb3136ee7ddd17fc015
Isabel An <isabel.an@wustl.edu>
2019-11-14 01:36:38 -0600

    got all my functions to work
    style checkmarks

--- script.js
+++ script.js
@@ -3,14 +3,26 @@ document.getElementById("add").addEventListener("click",post);
 
 //list -- show saved todos when refreshes
 var xhttp = new XMLHttpRequest();
+
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var todos = JSON.parse(this.responseText);
         console.log(todos);
+        var i;
+        for (i=0; i<todos.length; i++){
+          renderToDos(todos[i]);
+          if(todos[i].completed==true){
+            var span=document.getElementById(todos[i].id);
+            var p = span.childNodes[1];
+            p.className="toDoTextComplete";
+          }
+        }
     }
 };
-xhttp.open("GET", "https://api.kraigh.net/todos", true);
-xhttp.setRequestHeader("x-api-key", "026c07fc839dabeeafe789417067738eccfdf3cab77fd9f9fad7074257c64503");
+
+xhttp.open("GET", "https://cse204.work/todos", true);
+xhttp.setRequestHeader("x-api-key","62a83b-dd0595-259c51-78553e-b3f8de");
+xhttp.send();
 
 //function to render to dos
 function renderToDos(todo){
@@ -20,7 +32,7 @@ function renderToDos(todo){
 
   var checkbox=document.createElement("button");
   var checkboxText=document.createTextNode("Check");
-  checkbox.className="check";
+  checkbox.className="checkboxBefore";
   checkbox.appendChild(checkboxText);
   checkbox.addEventListener("click",complete); //calling update to change complete to true when clicked
 
@@ -60,55 +72,36 @@ function post(){
           console.log(this.responseText);
       }
   };
-  xhttp2.open("POST", "https://api.kraigh.net/todos", true);
+  xhttp2.open("POST", "https://cse204.work/todos", true);
   xhttp2.setRequestHeader("Content-type", "application/json");
-  xhttp2.setRequestHeader("x-api-key", "026c07fc839dabeeafe789417067738eccfdf3cab77fd9f9fad7074257c64503");
+  xhttp2.setRequestHeader("x-api-key", "62a83b-dd0595-259c51-78553e-b3f8de");
   xhttp2.send(JSON.stringify(data));
-  }
-
-  function get(){
-    // Setting variable for ToDo id
-    var id = this.id;
-    // Initalize AJAX Request
-    var xhttp2 = new XMLHttpRequest();
-    // Response handler
-    xhttp2.onreadystatechange = function() {
-        // Wait for readyState = 4 & 200 response
-        if (this.readyState == 4 && this.status == 200) {
-            // parse JSON response
-            var todo = JSON.parse(this.responseText);
-            console.log(todo);
-        } else if (this.readyState == 4) {
-            // this.status !== 200, error from server
-            console.log(this.responseText);
-        }
-    };
-    xhttp2.open("GET", "https://api.kraigh.net/todos/"+id, true);
-    xhttp2.setRequestHeader("Content-type", "application/json");
-    xhttp2.setRequestHeader("x-api-key", "026c07fc839dabeeafe789417067738eccfdf3cab77fd9f9fad7074257c64503");
-    xhttp2.send();
   }
 
   function complete(event){
     var a = event.target.parentNode.id;
     var data = {
-      completed: "true"
+      completed: true
     }
-    var xhttp3 = new XMLHttpRequest();
-    xhttp3.onreadystatechange = function() {
+    var xhttp5 = new XMLHttpRequest();
+    xhttp5.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         var todo = JSON.parse(this.responseText);
         console.log(todo);
-        //add in here to change the class after it is pressed and then
-        //style it differently in the css using a different class
+        if(todo.completed="true"){
+        var span=document.getElementById(a);
+        var p = span.childNodes[1];
+        p.className="toDoTextComplete";
+      }
       } else if (this.readyState == 4){
         console.log(this.responseText);
       }
-    xhttp3.open("PUT", "https://api.kraigh.net/todos/"+a, true);
-    xhttp3.setRequestHeader("Content-type", "application/json");
-    xhttp3.setRequestHeader("x-api-key", "026c07fc839dabeeafe789417067738eccfdf3cab77fd9f9fad7074257c64503");
-    xhttp2.send(JSON.stringify(data));
     }
+      xhttp5.open("PUT", "https://cse204.work/todos/"+a, true);
+      xhttp5.setRequestHeader("Content-type", "application/json");
+      xhttp5.setRequestHeader("x-api-key", "62a83b-dd0595-259c51-78553e-b3f8de");
+      xhttp5.send(JSON.stringify(data));
+
   }
 
   function deleteFromList(event){
@@ -128,8 +121,8 @@ function post(){
             console.log(this.responseText);
         }
     };
-    xhttp4.open("DELETE", "https://api.kraigh.net/todos/"+a, true);
+    xhttp4.open("DELETE", "https://cse204.work/todos/"+a, true);
     xhttp4.setRequestHeader("Content-type", "application/json");
-    xhttp4.setRequestHeader("x-api-key", "026c07fc839dabeeafe789417067738eccfdf3cab77fd9f9fad7074257c64503");
+    xhttp4.setRequestHeader("x-api-key", "62a83b-dd0595-259c51-78553e-b3f8de");
     xhttp4.send();
   }
